e6702e7e7b95f19ea4ab299c9f673a5c20f031fe
pl435 <PatriciaLai@myop.ac.nz>
2022-02-11 12:53:12 +1300

    Plugin updates

--- web/Script/slideshow2.js
+++ web/Script/slideshow2.js
@@ -26,3 +26,9 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+// Simple JS
+
+function teReo() {
+  document.getElementById("welcome").innerText = "Kia Ora!";
+}
